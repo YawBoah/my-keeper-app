1ce1f24670963f2f28fd7933c83b67fa49074259
YBoah <yawboahene.yb@gmail.com>
2024-02-22 02:46:29 +0000

    Modified the content of the note component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Note from './Components/Note';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Note />
      <Footer />
     </div>
   );
