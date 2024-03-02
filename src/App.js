2c1d366f46e6dd6ab24586a4e5c441074950299c
YBoah <yawboahene.yb@gmail.com>
2024-03-02 01:55:40 +0000

    Arrow and Map Function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Note from './Components/Note';
+import notes from './notes';
+
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Note />
-     <Note />
-     <Footer />
+      <Header />
+      {notes.map(noteList=>{
+        return (
+          <Note
+            key={noteList.id}
+            title={noteList.title}
+            content={noteList.content}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
